cb7f3e401974e647468645aa9fb2aefa2b14ea0a
shakyacoding <100017645+shakyacoding@users.noreply.github.com>
2024-06-12 17:30:36 +0530

    Add files via upload
    splash screen making code for android studio with index.html ( Html, Css, Js ) files and app icon

---
+++ splash screen making code for android studio with index.html files and app icon/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.notecounting;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+
+
+
+}
